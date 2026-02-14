e73d445bcb1fe6c893e64f48cf8fb4a5f561e9a5
Flyli0 <kirillrozkov653@gmail.com>
2026-02-14 17:22:42 +0500

    Finished labwork 4

--- Lab4/online-store/src/app/app.ts
+++ Lab4/online-store/src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
-
+import  {CommonModule} from '@angular/common';
+import {ProductList} from './product-list/product-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, ProductList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
