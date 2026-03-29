59d428abf90216bbe127a8df263ee89e9afdc4d8
Flyli0 <kirillrozkov653@gmail.com>
2026-03-29 23:07:21 +0500

    Added API JSON collection (OPENAPI)

--- lab8/lab8veb/lab8/api/urls.py
+++ lab8/lab8veb/lab8/api/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path, include
 from .views import (ProductView, CategoryView, ProductDetailView, CategoryDetailView, CategoryProductView, ProductViewSet,
-                    ProductsByCategoryListView)
+                    ProductsByCategoryListView, CategoriesViewSet)
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet, basename="product")
+router.register(r'categories', CategoriesViewSet, basename="category")
 
 urlpatterns = [
     #path('products/', ProductView.as_view()),
-    path('categories/', CategoryView.as_view()),
+    #path('categories/', CategoryView.as_view()),
     #path('products/<int:id>/', ProductDetailView.as_view()),
-    path('categories/<int:id>/', CategoryDetailView.as_view()),
-    path('categories/<int:id>/products/', CategoryProductView.as_view()),
+    #path('categories/<int:id>/', CategoryDetailView.as_view()),
+    #path('categories/<int:id>/products/', CategoryProductView.as_view()),
     path('', include(router.urls)),
 ]
